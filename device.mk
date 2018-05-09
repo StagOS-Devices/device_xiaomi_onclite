586c7962424fd815065f4fb3c56bf61555b1a906
Rashed Abdel-Tawab <rashed@linux.com>
2018-05-09 13:16:27 -0700

    onclite: Build vendor version of libstdc++
    Change-Id: Ie43c0a6b594ecfcff393efb8a3937f04af0ee0a4

--- device.mk
+++ device.mk
@@ -133,6 +133,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libgui_vendor \
+    libstdc++.vendor \
     Snap
 
 PRODUCT_COPY_FILES += \
